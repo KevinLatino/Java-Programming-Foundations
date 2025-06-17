76b301db4497bdd26bcb86867e3c6fc8361f3f9c
KevinLatino <latinokevin9@gmail.com>
2025-06-17 17:38:25 -0600

    feat: adding exercise 4, class 5

--- Exercises-Class-5/Ejercicio6/src/main/java/com/mycompany/ejercicio6/Ejercicio6.java
+++ Exercises-Class-5/Ejercicio6/src/main/java/com/mycompany/ejercicio6/Ejercicio6.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Ejercicio6 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         int[][] matriz = new int[4][4];
 
-        System.out.println("Ingrese los números para la matriz de 4x4:");
+        System.out.println("Ingrese los elementos de la matriz 4x4:");
 
-        for (int fila = 0; fila < 4; fila++) {
-            for (int columna = 0; columna < 4; columna++) {
-                System.out.print("Valor en posición [" + fila + "][" + columna + "]: ");
-                matriz[fila][columna] = scanner.nextInt();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Elemento en posición [" + i + "][" + j + "]: ");
+                matriz[i][j] = teclado.nextInt();
             }
         }
 
         System.out.println("\nDiagonal principal:");
         for (int i = 0; i < 4; i++) {
-            System.out.println(matriz[i][i]);
+            System.out.print(matriz[i][i] + " ");
         }
 
-        scanner.close();
+        teclado.close();
     }
 }
