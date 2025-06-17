1f2ccc1c526b66d98fd520a2dc8032b7693cd7d9
alejoquesada <77295986+alejandroqueru@users.noreply.github.com>
2025-06-17 11:31:33 -0600

    exercise 8 y 9

---
+++ Exercises-Class-5/Ejercicio9/src/main/java/com/mycompany/ejercicio9/Ejercicio9.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio9;
+
+/**
+ *
+ * @author aleja
+ */
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+        int[][] numeros = {{1,2,3,4},{5,6,7}};
+        numeros[1][2] = 9;
+        System.out.println(numeros[1][2]);
+    }
+}
